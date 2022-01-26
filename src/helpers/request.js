6dc8967dddc0e17adb25734757469d5527d59959
MichaelRights <melkumyan94@gmail.com>
2022-01-26 13:03:42 +0400

    start scripts added

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,9 +1,10 @@
+const HOST = "http://localhost:8080";
 export async function request(uri, method = "GET", body = null, headers = {}) {
   if (body) {
     body = JSON.stringify(body);
     headers["Content-Type"] = "application/json";
   }
-  const response = await fetch(uri, {
+  const response = await fetch(HOST + uri, {
     method,
     body,
     headers,
